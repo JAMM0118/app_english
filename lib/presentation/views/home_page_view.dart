54785fed0175789d104bb0cdc1e380768746839e
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2023-11-11 14:54:08 -0500

    Pequeños ajustes

--- lib/presentation/views/home_page_view.dart
+++ lib/presentation/views/home_page_view.dart
@@ -11,231 +11,289 @@ class MyHomePageView extends StatelessWidget {
     final colors = Theme.of(context).colorScheme;
     final height_ = MediaQuery.sizeOf(context).height;
     final ancho = MediaQuery.sizeOf(context).width;
-                  
+
     return SizedBox(
-    
-      height: height_ ,
+      height: height_,
       child: SingleChildScrollView(
-          physics: const AlwaysScrollableScrollPhysics(),
-    
-          child: SizedBox(
-            height:height_ < 700  ?  height_ + 150 : height_-75   ,
-            width: double.infinity,
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 70,
-                  right: -40,
-                  child: SlideInRight(
-                      duration: const Duration(seconds: 3),
-                      child: SizedBox(
-                          height: 100,
-                          width: 200,
-                          child: Image.asset("assets/images/game.gif"))),
-                ),
-                
-                Positioned(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: SizedBox(
+          height: height_ < 700 ? height_ + 150 : height_ - 75,
+          width: double.infinity,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 70,
+                right: -40,
+                child: SlideInRight(
+                    duration: const Duration(seconds: 3),
+                    child: SizedBox(
+                        height: 100,
+                        width: 200,
+                        child: Image.asset("assets/images/game.gif"))),
+              ),
+              Positioned(
                   bottom: 10,
                   left: 160,
-                  child: ShakeX(
-                    infinite: true,
+                  child: _AnimationMoveX(
+                    always: true,
+                    height: 70,
+                    width: 60,
                     duration: const Duration(seconds: 10),
-                    from:(MediaQuery.sizeOf(context).width)/3,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: const Color.fromARGB(255, 214, 109, 24),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
+                    from: (MediaQuery.sizeOf(context).width) / 3,
+                    color: const Color.fromARGB(255, 214, 109, 24),
+                  )),
+              Positioned(
                   bottom: 220,
                   left: 10,
-                  child: ShakeY(
-                    infinite: true,
-                    duration: const Duration(seconds: 10),
-                    from: -((MediaQuery.sizeOf(context).width ) / 3),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: Colors.blueAccent,
-                      ),
-                    ),
+                  child: _AnimationMoveY(
+                      always: true,
+                      height: 70,
+                      width: 60,
+                      color: Colors.blueAccent,
+                      duration: const Duration(seconds: 10),
+                      from: -((MediaQuery.sizeOf(context).width) / 3))),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: SlideInLeft(
+                  child: const _AnimationDance(
+                    always: true,
+                    height: 70,
+                    width: 60,
+                    color: Colors.redAccent,
+                    duration: Duration(seconds: 2),
                   ),
                 ),
-                 Positioned(
-                  bottom: 0,
-                  left: 0,
-                  child: SlideInLeft(
-                    child: Dance(
-                      infinite: true,
-                      duration: const Duration(seconds: 2),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: Colors.redAccent,
-                      ),
-                    ),
-                  ),
-                ),
-                
-                Positioned(
+              ),
+              Positioned(
                   top: 270,
                   left: 10,
-                  child: ShakeY(
-                    infinite: true,
+                  child: _AnimationMoveY(
+                      always: true,
+                      height: 70,
+                      width: 60,
+                      color: colors.primary,
+                      duration: const Duration(seconds: 10),
+                      from: 100)),
+              Positioned(
+                top: 220,
+                right: 100,
+                child: _AnimationMoveX(
+                    always: true,
+                    height: 70,
+                    width: 60,
+                    color: Colors.green,
                     duration: const Duration(seconds: 10),
-                    from: 100,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: colors.primary,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 220,
-                  right: 100,
-                  child: ShakeX(
-                    infinite: true,
-                    duration: const Duration(seconds: 10),
-                    from: (MediaQuery.sizeOf(context).width * .5) / 2,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
+                    from: (MediaQuery.sizeOf(context).width * .5) / 2),
+              ),
+              const Positioned(
                   bottom: 70,
                   right: 10,
-                  child: ShakeY(
-                    infinite: true,
-                    duration: const Duration(seconds: 10),
-                    from: 100,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: Colors.greenAccent,
-                      ),
-                    ),
-                  ),
+                  child: _AnimationMoveY(
+                      always: true,
+                      height: 70,
+                      width: 60,
+                      color: Colors.greenAccent,
+                      duration: Duration(seconds: 10),
+                      from: 100)),
+              Positioned(
+                top: 80,
+                left: 20,
+                child: SlideInRight(
+                    child: const _AnimationDance(
+                  always: true,
+                  duration: Duration(seconds: 2),
+                  height: 70,
+                  width: 60,
+                  color: Colors.amber,
+                )),
+              ),
+              Positioned(
+                bottom: 290,
+                right: 0,
+                child: SlideInLeft(
+                  child: const _AnimationDance(
+                      always: true,
+                      height: 70,
+                      width: 60,
+                      color: Colors.deepPurpleAccent,
+                      duration: Duration(seconds: 2)),
                 ),
-                
-                Positioned(
-                  top: 80,
-                  left: 20,
-                  child: SlideInRight(
-                    child: Dance(
-                      infinite: true,
-                      duration: const Duration(seconds: 2),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: Colors.amber,
-                      ),
-                    ),
-                  ),
-                ),
-                  Positioned(
-                  bottom: 290,
-                  right: 0,
-                  child: SlideInLeft(
-                    child: Dance(
-                      infinite: true,
-                      duration: const Duration(seconds: 2),
-                      child: Container(
-                        height: 70,
-                        width: 60,
-                        color: Colors.deepPurpleAccent,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: MediaQuery.sizeOf(context).width * .5 - 50,
-                  top: 245,
-                  child: ElasticInRight(
+              ),
+              Positioned(
+                left: MediaQuery.sizeOf(context).width * .5 - 50,
+                top: 245,
+                child: ElasticInRight(
                     duration: const Duration(seconds: 3),
-                    child: ShakeX(
-                        duration: const Duration(seconds: 30),
-                        infinite: true,
-                        from: MediaQuery.sizeOf(context).width * .5 - 50,
-                        child: SizedBox(
-                            height: 40,
-                            width: 100,
-                            child: Image.asset("assets/images/R.gif"))),
-                  ),
-                ),
-                Positioned(
-                  right: (ancho * .5 - 50),
-                  bottom: 10,
-                  child: ElasticInRight(
+                    child: _AnimationMoveX(
+                      always: true,
+                      height: 40,
+                      width: 100,
+                      duration: const Duration(seconds: 30),
+                      from: MediaQuery.sizeOf(context).width * .5 - 50,
+                      child: Image.asset("assets/images/R.gif"),
+                    )),
+              ),
+              Positioned(
+                right: (ancho * .5 - 50),
+                bottom: 10,
+                child: ElasticInRight(
                     duration: const Duration(seconds: 3),
-                    child: ShakeX(
-                        duration: const Duration(seconds: 30),
-                        infinite: true,
-                        from: -(MediaQuery.sizeOf(context).width * .5 - 50),
-                        child: SizedBox(
-                            height: 90,
-                            width: 100,
-                            child: Image.asset("assets/images/spiderman_dancing.gif"))),
-                  ),
-                ),
-                Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: height_ <=900 ?  MainAxisAlignment.start : MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.only(left: 20, top: 70),
-                          child: Text("Time to play",
-                              textAlign: TextAlign.center,
-                              style: GoogleFonts.getFont(
-                                'Press Start 2P',
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                                color: colors.primary,
-                              ))),
-                      const Padding(
-                        padding: EdgeInsets.only(left: 20, top: 40),
-                        child: Text(
-                          "Choose your tense",
-                          style: TextStyle(
-                              fontSize: 20,
+                    child: _AnimationMoveX(
+                      always: true,
+                      height: 90,
+                      width: 100,
+                      duration: const Duration(seconds: 30),
+                      from: -(MediaQuery.sizeOf(context).width * .5 - 50),
+                      child: Image.asset("assets/images/spiderman_dancing.gif"),
+                    )),
+              ),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: height_ <= 900
+                      ? MainAxisAlignment.start
+                      : MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(left: 20, top: 70),
+                        child: Text("Time to play",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.getFont(
+                              'Press Start 2P',
+                              fontSize: 30,
                               fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                        ),
+                              color: colors.primary,
+                            ))),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 20, top: 40),
+                      child: Text(
+                        "Choose your tense",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
                       ),
-                      const SizedBox(height: 20,),
-                      const _StandsScreen(
-                          image: "assets/images/fondoFuturama.png", titulo: "Past tense / Future tense"),
-                      
-                      const SizedBox(height: 20,),
-                      const _StandsScreen(
-                          image: "assets/images/presente.jpg",
-                          titulo: "Present tense"),
-                      ],
-                  ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const _StandsScreen(
+                        image: "assets/images/fondoFuturama.png",
+                        titulo: "Past tense / Future tense"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const _StandsScreen(
+                        image: "assets/images/presente.jpg",
+                        titulo: "Present tense"),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _AnimationMoveX extends StatelessWidget {
+  final bool always;
+  final double height;
+  final double width;
+  final Color? color;
+  final Duration duration;
+  final double from;
+  final Widget? child;
+
+  const _AnimationMoveX(
+      {this.child,
+      required this.always,
+      required this.height,
+      required this.width,
+      this.color,
+      required this.duration,
+      required this.from});
+
+  @override
+  Widget build(BuildContext context) {
+    return ShakeX(
+      infinite: always,
+      duration: duration,
+      from: from,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          height: height,
+          width: width,
+          color: color,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
+class _AnimationMoveY extends StatelessWidget {
+  final bool always;
+  final double height;
+  final double width;
+  final Color color;
+  final Duration duration;
+  final double from;
+
+  const _AnimationMoveY(
+      {required this.always,
+      required this.height,
+      required this.width,
+      required this.color,
+      required this.duration,
+      required this.from});
+
+  @override
+  Widget build(BuildContext context) {
+    return ShakeY(
+      infinite: always,
+      duration: duration,
+      from: from,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          height: height,
+          width: width,
+          color: color,
+        ),
+      ),
+    );
+  }
+}
+
+class _AnimationDance extends StatelessWidget {
+  final bool always;
+  final double height;
+  final double width;
+  final Color color;
+  final Duration duration;
+
+  const _AnimationDance({
+    required this.always,
+    required this.height,
+    required this.width,
+    required this.color,
+    required this.duration,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dance(
+      infinite: always,
+      duration: duration,
+      child: Container(
+        height: height,
+        width: width,
+        color: color,
+      ),
     );
   }
 }
@@ -268,10 +326,10 @@ class _StandsScreen extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
               child: GestureDetector(
                 onTap: () => context.push('/standsScreen', extra: {
-                  'image-background': titulo =="Present tense"
+                  'image-background': titulo == "Present tense"
                       ? "assets/images/presentito.png"
-                      :  "assets/images/picapiedrita.png",
-                    'titulo': titulo,
+                      : "assets/images/picapiedrita.png",
+                  'titulo': titulo,
                 }),
                 child: SizedBox(
                   height: 150,
--- lib/widgets/shared/loading_screen.dart
+++ lib/widgets/shared/loading_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:go_router/go_router.dart';
 
 class LoadingScreen extends StatelessWidget {
@@ -7,7 +6,7 @@ class LoadingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    FlutterNativeSplash.remove();
+    //FlutterNativeSplash.remove();
     Future.delayed(const Duration(milliseconds: 6350), () {
       context.go('/bienvenidaScreen');
     });
